5205ce5052e6d53a8565176278d968a2457ca350
K3vnDev <kevinrpolo16@gmail.com>
2025-06-21 22:20:50 -0500

    enhance error handling in TasksSection and update  README

---
+++ README.md
@@ -0,0 +1,5 @@
+## 📜 Listify
+
+A simple CRUD web application to manage your tasks, allowing you to organize them into separate lists.
+
+It was built mostly for personal use and practicing both Next.js and Supabase workflows. This is an enhanced version of [Todo App](https://github.com/K3vnDev/todo-app), one of my first web applications.
--- src/components/tasks/TasksSection.tsx
+++ src/components/tasks/TasksSection.tsx
@@ -1,38 +1,56 @@
-import { LoadingIcon } from '@/icons'
+import { LoadingIcon, ReloadIcon } from '@/icons'
 import { useTasksStore } from '@/store/tasks/useTasksStore'
 import type { Task as TaskType } from '@/types'
 import { dataFetch } from '@/utils/dataFetch'
 import { Task } from '@components/tasks/Task'
 import { useParams } from 'next/navigation'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 export const TasksSection = () => {
   const setTasks = useTasksStore(s => s.setTasks)
   const tasks = useTasksStore(s => s.tasks)
+  const [isOnError, setIsOnError] = useState(false)
 
   const { listId } = useParams()
 
-  const handleError = () => {
-    // TODO: Handle error
-  }
+  const loadTasks = () => {
+    setIsOnError(false)
 
-  useEffect(() => {
     dataFetch<TaskType[]>({
       url: `/api/tasks?list-id=${listId}`,
       onSuccess: data => setTasks(data),
-      onError: handleError
+      onError: () => setIsOnError(true)
     })
-  }, [])
+  }
+
+  // Load tasks on mount
+  useEffect(loadTasks, [])
+
+  if (isOnError) {
+    return (
+      <MessageWrapper>
+        <Message text='Error loading tasks.' className='text-red-600 font-bold' />
+        <button
+          className='py-2 px-4 bg-red-500/40 border border-red-500 rounded-md w-fit flex items-center gap-2'
+          onClick={loadTasks}
+        >
+          <ReloadIcon className='text-red-600 size-6' />
+          <Message text='Try again' className='text-red-600 font-semibold' />
+        </button>
+      </MessageWrapper>
+    )
+  }
 
   if (tasks === null)
     return <LoadingIcon className='animate-spin mx-auto my-2 size-12 text-zinc-500' />
 
-  if (tasks.length === 0)
+  if (tasks.length === 0) {
     return (
-      <span className='h-12 mt-4 w-full flex flex-col items-center justify-center text-zinc-500'>
-        You dont have any tasks yet.
-      </span>
+      <MessageWrapper>
+        <Message text='You dont have any tasks yet.' />
+      </MessageWrapper>
     )
+  }
 
   return (
     <ul className='flex flex-col gap-2 h-full overflow-y-scroll overflow-x-hidden'>
@@ -42,3 +60,16 @@ export const TasksSection = () => {
     </ul>
   )
 }
+
+interface MessageProps {
+  className?: string
+  text: string
+}
+
+const Message = ({ className, text: message }: MessageProps) => (
+  <span className={`text-zinc-500 ${className}`}>{message}</span>
+)
+
+const MessageWrapper = ({ children }: { children: React.ReactNode }) => (
+  <div className='flex flex-col items-center justify-center h-full gap-4'>{children}</div>
+)
